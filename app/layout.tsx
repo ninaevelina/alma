62cd87295d389c5c388ab7a5ab52591d75127da0
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2024-04-11 13:45:48 +0200

    (refactor): updates import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Nav from "@/ui/shared/nav";
+import Nav from "@/ui/shared/nav/nav";
 import { CatProvider } from "@/contexts/CatContext";
 
 const inter = Inter({ subsets: ["latin"] });
