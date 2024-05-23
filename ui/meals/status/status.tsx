a4152a4db52723fb616359e9eccb46cb9ccd5ddf
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2024-05-23 10:08:27 +0200

    refactor: adds images to display status

--- ui/meals/status/status.tsx
+++ ui/meals/status/status.tsx
@@ -2,59 +2,65 @@
 
 import { useCat } from "@/contexts/CatContext";
 import Image from "next/image";
-import almaImage from "@/public/alma.png";
+import almaImage from "@/public/alma-cropped.png";
 import styles from "./status.module.scss";
+import bubbleFull from "@/public/pixel-speech-bubble-2.png";
+import bubbleHungry from "@/public/pixel-speech-bubble.png";
 
 export const Status = () => {
   const { lastFed } = useCat();
   const date = new Date();
-  const difference = date.valueOf() - lastFed?.valueOf();
+  const difference = date.valueOf() - lastFed?.time.valueOf();
   const differenceInHrs = difference / 1000 / 60 / 60;
 
   const showHidePrompt = () => {
     //Todo: change image of alma when hungry
     if (differenceInHrs > 4) {
       return (
-        <div>
-          <div className={styles.imageContainer}>
-            {" "}
+        <div className={styles.imageContainer}>
+          <div className={styles.imageCat}>
             <Image
               src={almaImage.src}
-              height={341}
-              width={256}
+              height={240}
+              width={140}
               alt="Image of Alma"
             />
+            <div className={styles.imageBubble}>
+              <Image
+                src={bubbleHungry.src}
+                height={100}
+                width={140}
+                alt="Chat bubble"
+              />
+            </div>
           </div>
-          <p className={styles.imageText}>Feed me, I am hungry</p>
         </div>
       );
     } else {
       return (
-        <div>
-          <div className={styles.imageContainer}>
+        <div className={styles.imageContainer}>
+          <div className={styles.imageCat}>
             <Image
               src={almaImage.src}
-              height={341}
-              width={256}
+              height={240}
+              width={140}
               alt="Image of Alma"
             />
           </div>
-          <p className={styles.imageText}>I am full, no food for me thanks.</p>{" "}
+          <div className={styles.imageBubble}>
+            <Image
+              src={bubbleFull.src}
+              height={40}
+              width={140}
+              alt="Chat bubble"
+            />
+          </div>
         </div>
       );
     }
   };
 
-  return (
-    <div className={styles.center}>
-      <div className={styles.statusText}>
-        <p>
-          Last fed: {lastFed ? lastFed.toLocaleTimeString() : "Not fed yet"}
-        </p>
-      </div>
-      {showHidePrompt()}
-    </div>
-  );
+  return <div className={styles.status}>{showHidePrompt()}</div>;
 };
 
 export default Status;
