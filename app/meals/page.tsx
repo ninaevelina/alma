0ddf42d6ce4641f11c5874d4264ffe848058e180
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2024-05-23 15:11:04 +0200

    refactor: implements grid and updates styles for main-el

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -1,14 +1,12 @@
-import Button from "@/ui/meals/button/button";
+import Status from "@/ui/meals/status/status";
 import styles from "./page.module.scss";
 import FeedForm from "@/ui/meals/form/feed-form";
 
 export default async function Page() {
   return (
-    <>
-      <section className={styles.column}>
-        <Button />
-        <FeedForm />
-      </section>
-    </>
+    <section className={styles.grid}>
+      <FeedForm />
+      <Status />
+    </section>
   );
 }
